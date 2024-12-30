96f9edf24bb62f1be25987488612f39350e9011c
glenn <glennerichall@hotmail.com>
2024-12-29 20:06:58 -0500

    added service keys as symbols

--- application/services/serviceKeys.mjs
+++ application/services/serviceKeys.mjs
@@ -1,8 +1,8 @@
-export const s_roleDAO = "roleDAO";
-export const s_ruleDAO = "ruleDAO";
-export const s_apiKeyDAO = "apiKeyDAO";
-export const s_userDAO = "userDAO";
-export const s_authDAO = "authDAO";
-export const s_preferenceDAO = "preferenceDAO";
-export const s_accessDao = "accessDao";
-export const s_logins = "logins";
+export const s_roleDAO = Symbol("roleDAO");
+export const s_ruleDAO = Symbol("ruleDAO");
+export const s_apiKeyDAO = Symbol("apiKeyDAO");
+export const s_userDAO = Symbol("userDAO");
+export const s_authDAO = Symbol("authDAO");
+export const s_preferenceDAO = Symbol("preferenceDAO");
+export const s_accessDao = Symbol("accessDao");
+export const s_logins = Symbol("logins");
