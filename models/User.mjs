e504a6ed0d5b2fe7733cb7dac214260373274884
Glenn Hall <glennerichall@hotmail.com>
2025-01-09 10:35:37 -0500

    changed private fields to symbols

--- models/User.mjs
+++ models/User.mjs
@@ -1,49 +1,53 @@
 import {getUserDAO} from "../application/services/services.mjs";
 
+const kp_data = Symbol();
+const kp_loaded = Symbol();
+const kp_apiKeys = Symbol();
+const kp_roles = Symbol();
+
 export class User {
-    #data;
-    #loaded;
-    #apiKeys;
-    #roles;
 
     constructor(data) {
-        this.#data = data;
+        this[kp_data] = data;
+        this[kp_loaded] = null;
+        this[kp_apiKeys] = null;
+        this[kp_roles] = null;
     }
 
     async save() {
-        await getUserDAO(this).saveOne(this.#data);
+        await getUserDAO(this).saveOne(this[kp_data]);
     }
 
     async load() {
-        this.#loaded = getUserDAO(this).loadOne(this.#data);
+        this[kp_loaded] = getUserDAO(this).loadOne(this[kp_data]);
     }
 
     async getApiKeys() {
-        if (!this.#apiKeys) {
-            this.#apiKeys = await getUserDAO(this).getApiKeys(this.#loaded);
+        if (!this[kp_apiKeys]) {
+            this[kp_apiKeys] = await getUserDAO(this).getApiKeys(this[kp_loaded]);
         }
-        return this.#apiKeys;
+        return this[kp_apiKeys];
     }
 
     async getRoles() {
-        if (!this.#roles) {
-            this.#roles = await getUserDAO(this).getRoles(this.#loaded);
+        if (!this[kp_roles]) {
+            this[kp_roles] = await getUserDAO(this).getRoles(this[kp_loaded]);
         }
-        return this.#roles;
+        return this[kp_roles];
     }
 
     async getRules() {
-        if (!this.#roles) {
-            this.#roles = await getUserDAO(this).getAclRules(this.#loaded);
+        if (!this[kp_roles]) {
+            this[kp_roles] = await getUserDAO(this).getAclRules(this[kp_loaded]);
         }
-        return this.#roles;
+        return this[kp_roles];
     }
 
     async getPreferences() {
-        if (!this.#roles) {
-            this.#roles = await getUserDAO(this).getPreferences(this.#loaded);
+        if (!this[kp_roles]) {
+            this[kp_roles] = await getUserDAO(this).getPreferences(this[kp_loaded]);
         }
-        return this.#roles;
+        return this[kp_roles];
     }
 
 }
